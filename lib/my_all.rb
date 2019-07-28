bdc17afb2c560def1afe3bb3134337630c264938
David Strauch <dmaster18@gmail.com>
2019-07-28 18:32:20 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,10 +7,10 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i+=1
   end
-  if block_return_values.include?(FALSE)
-    FALSE
+  if block_return_values.include?(false)
+    false
   else  
-    TRUE
+    true
   end
 end
 
